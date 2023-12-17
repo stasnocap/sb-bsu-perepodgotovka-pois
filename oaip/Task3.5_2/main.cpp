0b624a8e594276f4a61aa266c1aedfde8ea62d8d
Stas <stasnocap@gmail.com>
2023-12-17 18:53:41 +0300

    add OAiP Task3.5_2

---
+++ oaip/Task3.5_2/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <math.h>;
+#include <iostream>
+
+int main() {
+    const double x{0.1722};
+    const double y{6.33};
+    const double z{3.25e-4};
+
+    std::cout << 5 * atan(x) - acos(x) / 4 * (x + 3 * fabs(x - y) + x * x) / (fabs(x - y) * z + x * x);
+
+    return 0;
+}
