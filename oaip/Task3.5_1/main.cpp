e57dc8b14c3172b71bc9a885f19df48dd9fb811d
Stas <stasnocap@gmail.com>
2023-12-17 18:52:27 +0300

    add OAiP Task3.5_1

---
+++ oaip/Task3.5_1/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <math.h>;
+#include <iostream>
+
+int main() {
+    const double percent{10};
+    const double loanAmount{123};
+    const int paymentsPerYear{2};
+    const int countOfYears{1};
+
+    std::cout << (percent * loanAmount / paymentsPerYear) /
+                 pow(1 - ((percent / paymentsPerYear) + 1), -paymentsPerYear * countOfYears);
+
+    return 0;
+}
