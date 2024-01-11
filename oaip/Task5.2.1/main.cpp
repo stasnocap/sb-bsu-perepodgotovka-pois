30cfc7c6f4ce3f2a9833f3ee74aaf7a2f0d230aa
Stas <stasnocap@gmail.com>
2024-01-11 06:40:51 +0300

    add oaip Task5.2.1

---
+++ oaip/Task5.2.1/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <array>
+#include<numeric>
+
+#define GET_VARIABLE_NAME(Variable) (#Variable)
+
+int main() {
+    constexpr std::array A{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    constexpr std::array B{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    const int aSum{std::accumulate(std::begin(A), std::end(A), 0)};
+    const int bSum{std::accumulate(std::begin(B), std::end(B), 0)};
+
+    std::cout << ((aSum < bSum) ? GET_VARIABLE_NAME(A) : GET_VARIABLE_NAME(B));
+
+    return EXIT_SUCCESS;
+}
