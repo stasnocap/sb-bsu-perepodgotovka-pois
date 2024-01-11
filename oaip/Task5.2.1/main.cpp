795e0e73bde1d330d1b99e60b7914e8388d84dca
Stas <stasnocap@gmail.com>
2024-01-11 06:42:36 +0300

    Task5.2.1: make sums constexpr

--- oaip/Task5.2.1/main.cpp
+++ oaip/Task5.2.1/main.cpp
@@ -8,8 +8,8 @@ int main() {
     constexpr std::array A{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     constexpr std::array B{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-    const int aSum{std::accumulate(std::begin(A), std::end(A), 0)};
-    const int bSum{std::accumulate(std::begin(B), std::end(B), 0)};
+    constexpr int aSum{std::accumulate(std::begin(A), std::end(A), 0)};
+    constexpr int bSum{std::accumulate(std::begin(B), std::end(B), 0)};
 
     std::cout << ((aSum < bSum) ? GET_VARIABLE_NAME(A) : GET_VARIABLE_NAME(B));
 
