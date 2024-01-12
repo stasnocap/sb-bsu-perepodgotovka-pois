d97272d89ec7e86adaee44367813e7a88d318ce6
Стас <stasnocap@gmail.com>
2024-01-12 09:07:59 +0300

    add oaip Task5.2.3

---
+++ oaip/Task5.2.3/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+
+int main() {
+    std::vector<int> array = {8, 4, 5, 9};
+
+    std::sort(array.begin(), array.end(), std::less{});
+
+    array.erase(array.begin());
+    array.erase(array.end());
+
+    std::cout <<
+              (double) std::accumulate(array.begin(), array.end(), 0)
+              /
+              (double) std::ssize(array);
+
+    return EXIT_SUCCESS;
+}
