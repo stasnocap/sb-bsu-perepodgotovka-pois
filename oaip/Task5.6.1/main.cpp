e5d859c2a2d88e1ea669018d16b728f8ced1ed94
Stas <stasnocap@gmail.com>
2024-01-13 14:31:23 +0300

    add oaip task 5.6.1

---
+++ oaip/Task5.6.1/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <array>
+
+template<typename T, std::size_t Row, std::size_t Col>
+void printArray(std::array<std::array<T, Col>, Row> &array) {
+    for (std::array<T, Col> &row: array) {
+        for (T &col: row) {
+            std::cout << col << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
+int main() {
+    constexpr int arraySize{5};
+
+    std::array<std::array<int, arraySize>, arraySize> array{};
+
+    for (int i = 0; i < array.size(); ++i) {
+        array[i][i] = 1;
+    }
+
+    printArray(array);
+
+    return 0;
+}
