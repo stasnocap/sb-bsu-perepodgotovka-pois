c481219f1ca8562f37c6548156e352b59a6ba513
Стас <stasnocap@gmail.com>
2024-04-26 09:05:16 +0300

    add users, liked tracks, followers to ddl

--- oipdb/Music/Console/DDL.sql
+++ oipdb/Music/Console/DDL.sql
@@ -31,3 +31,38 @@ create table "Tracks"
             on delete cascade,
     "Name"     varchar(255) not null
 );
+
+create table "Users"
+(
+    "Id"   bigint generated always as identity
+        primary key,
+    "Name" varchar(255) not null
+);
+
+create table "LikedTracks"
+(
+    "Id"       bigint generated always as identity
+        primary key,
+    "TrackId" bigint
+        constraint fk_track
+            references "Tracks"
+            on delete cascade,
+    "UserId"   bigint
+        constraint fk_user
+            references "Users"
+            on delete cascade
+);
+
+create table "Followers"
+(
+    "Id"       bigint generated always as identity
+        primary key,
+    "ArtistId" bigint
+        constraint fk_artist
+            references "Artists"
+            on delete cascade,
+    "UserId"   bigint
+        constraint fk_user
+            references "Users"
+            on delete cascade
+);
